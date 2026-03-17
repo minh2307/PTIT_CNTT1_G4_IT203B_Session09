1c76a95048789af970459f2b5d0e7a39c463cb6a
phamvietquan <phamquanlv123.2986@gmail.com>
2026-03-17 22:13:30 +0700

    done

--- src/main/java/org/example/entity/Vehicle.java
+++ src/main/java/org/example/entity/Vehicle.java
@@ -1,4 +1,47 @@
 package org.example.entity;
 
-public abstract class Vehicle {
+
+import java.util.UUID;
+
+public abstract class Vehicle implements Runnable {
+
+    protected String id;
+    protected double speed;
+    protected String type;
+    protected int priority;
+
+    public Vehicle(double speed, String type, int priority) {
+        this.id = UUID.randomUUID().toString();
+        this.speed = speed;
+        this.type = type;
+        this.priority = priority;
+    }
+
+    public abstract void move();
+
+    public String getId() {
+        return id;
+    }
+
+    public double getSpeed() {
+        return speed;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public int getPriority() {
+        return priority;
+    }
+
+    @Override
+    public void run() {
+        move();
+    }
+
+    @Override
+    public String toString() {
+        return "[" + type + " #" + id.substring(0, 5) + "]";
+    }
 }
